1ed52b117411c21ffabcb91d9f7368ab725cf15f
Seunghun Lee <waydi1@gmail.com>
2016-12-10 17:06:57 -0800

    Move clinet keys to all config

--- catalog/config.py
+++ catalog/config.py
@@ -2,13 +2,13 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'test_secret_key'
 
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://catalog:catalog@localhost:5432/catalog'
-
     GOOGLE_CLIENT_ID='YOUR_GOOGLE_CLIENT_ID'
     GOOGLE_CLIENT_SECRET='YOUR_GOOGLE_CLIENT_SECRET'
     FB_CLIENT_ID='YOUR_FACEBOOK_CLIENT_ID'
     FB_CLIENT_SECRET='YOUR_FACEBOOK_CLIENT_SECRET'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://catalog:catalog@localhost:5432/catalog'
 
 class DevConfig(Config):
     DEBUG = True

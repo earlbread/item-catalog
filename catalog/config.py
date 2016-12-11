b4b18f7b7c602c21fc03bc885efcf5fece77f37e
Seunghun Lee <waydi1@gmail.com>
2016-12-10 23:31:05 -0800

    Update configuration for heroku deployment

--- catalog/config.py
+++ catalog/config.py
@@ -1,14 +1,19 @@
+import os
+
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'test_secret_key'
 
-    GOOGLE_CLIENT_ID='YOUR_GOOGLE_CLIENT_ID'
-    GOOGLE_CLIENT_SECRET='YOUR_GOOGLE_CLIENT_SECRET'
-    FB_CLIENT_ID='YOUR_FACEBOOK_CLIENT_ID'
-    FB_CLIENT_SECRET='YOUR_FACEBOOK_CLIENT_SECRET'
+    GOOGLE_CLIENT_ID     = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    FB_CLIENT_ID         = os.environ.get('FB_CLIENT_ID')
+    FB_CLIENT_SECRET     = os.environ.get('FB_CLIENT_SECRET')
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://catalog:catalog@localhost:5432/catalog'
+
+class HerokuConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevConfig(Config):
     DEBUG = True
